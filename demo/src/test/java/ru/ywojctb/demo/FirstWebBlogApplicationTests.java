89b525e576a40d1c6b97629aa2b6d55c6d6e2cbc
bkirilll <kirya.bulekov@mail.ru>
2023-11-08 22:51:12 +0300

    main page added

---
+++ demo/src/test/java/ru/ywojctb/demo/FirstWebBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ywojctb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
