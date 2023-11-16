a40aa265ae631aedd5e7d6bfd4575c6bb642a51e
bkirilll <kirya.bulekov@mail.ru>
2023-11-16 23:25:01 +0300

    PostgreSQL and repositories added

--- buysell/src/main/java/com/example/buysell/models/Product.java
+++ buysell/src/main/java/com/example/buysell/models/Product.java
@@ -1,26 +1,40 @@
 package com.example.buysell.models;
 
 
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Controller;
 
+@Entity
+@Table(name = "products")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     Long id;
 
+    @Column(name = "title")
     String title;
 
+    @Column(name = "description", columnDefinition = "text")
     String description;
 
+    @Column(name = "price")
     int price;
 
+    @Column(name = "city")
     String city;
 
+    @Column(name = "author")
     String author;
 
 }
