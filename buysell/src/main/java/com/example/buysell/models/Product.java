95cc864b641ba1c011a94b153f6fe9c621f5980b
bkirilll <kirya.bulekov@mail.ru>
2023-11-16 22:49:46 +0300

    model, service and pages added

---
+++ buysell/src/main/java/com/example/buysell/models/Product.java
@@ -0,0 +1,26 @@
+package com.example.buysell.models;
+
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Product {
+
+    Long id;
+
+    String title;
+
+    String description;
+
+    int price;
+
+    String city;
+
+    String author;
+
+}
---
+++ buysell/src/main/java/com/example/buysell/services/ProductService.java
@@ -0,0 +1,41 @@
+package com.example.buysell.services;
+
+import com.example.buysell.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private List<Product> products = new ArrayList<>();
+
+    private long ID = 0;
+
+    {
+        products.add(new Product(++ID, "Iphone 10", "description", 80000, "Moscow", "Oleg"));
+        products.add(new Product(++ID, "Iphone 9", "description4", 70000, "Moscow", "Ivan"));
+    }
+
+    public List<Product> listProducts() {
+        return products;
+    }
+
+    public void saveProduct(Product product) {
+        product.setId(++ID);
+        products.add(product);
+    }
+
+    public void deleteProduct(Long id) {
+        products.removeIf(product -> product.getId().equals(id));
+    }
+
+
+    public Product getProductById(Long id) {
+        for (Product product : products) {
+            if (product.getId().equals(id)) return product;
+        }
+        return null;
+    }
+}
